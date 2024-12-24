829d97245844b4dc68db909357baa289f90e174b
AdTanya <suhanova59_net@mail.ru>
2024-12-24 19:39:11 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,13 @@
+username=Таня
+title=Заметка
+content=Важно
+status=Исполнено
+created_date=24-12-2024
+issue_date=24-12-2024
+
+print("Таня",username)
+print("Заметка",title)
+print("Важно",content)
+print("Исполнено",status)
+print("24-12-2024",created_date)
+print("24-12-2024",issue_date)
